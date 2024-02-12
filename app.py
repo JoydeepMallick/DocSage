daaf5f40c283ce64fd946d87cfc8e98acadd39d6
Joydeep Mallick <joydeep150703@gmail.com>
2024-02-12 18:49:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -26,7 +27,39 @@ def get_text_from_pdf(pdf_files):
             text += page.extract_text()
     return text
 
-deg get_chunks(text):
+def get_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
-    chunks = text_spliter.split_text(text)
+    chunks = text_splitter.split_text(text)
     return chunks
+
+def get_vector_store(text_chunks):
+    embedding = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from provided context, make sure to provide all the details, if the answer is not in the provided context just say "answer is not available in the context", don't provide the wrong answer.
+    Context: \n {context}\n
+    Question: \n {question}\n
+
+    Answer : 
+    """
+    model = ChatGoogleGenerativeAI(model = 'gemini-pro', temperature=0.3)
+    prompt = PromptTemplate(template = prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents":docs, "question": user_question},
+        return_only_outputs = True
+    )
+
